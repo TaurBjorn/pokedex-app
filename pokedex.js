1f3aa5ac68b710106b49955a958d4bf5d15dfcbe
Bjorn Vanmarsenille <bjorn.vanmarsenille90@gmail.com>
2022-07-15 19:44:10 +0200

    cloned a parent/child element and added two more pokemon with DOM-manipulation

--- pokedex.js
+++ pokedex.js
@@ -1,2 +1,16 @@
+
+const pokedex = document.querySelector('.pokedex');
 const entry = document.querySelector('.pokedex-entry');
-console.dir(entry);
+
+
+const cloneOne = entry.cloneNode(true);
+cloneOne.querySelector('.number').innerText = "#002";
+cloneOne.querySelector('.name').innerText = "Ivysaur";
+cloneOne.querySelector('.img-container img').src = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png";
+pokedex.appendChild(cloneOne);
+
+const cloneTwo = entry.cloneNode(true);
+cloneTwo.querySelector('.number').innerText = "#003";
+cloneTwo.querySelector('.name').innerText = "Venusaur";
+cloneTwo.querySelector('.img-container img').src = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png";
+pokedex.appendChild(cloneTwo);
