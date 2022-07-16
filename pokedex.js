fe102458ae1fbac6040c16273c662c5221c4549c
Bjorn Vanmarsenille <bjorn.vanmarsenille90@gmail.com>
2022-07-16 20:12:45 +0200

    First glimpse of the pokedex, next 140 pokemon  to be added

--- pokedex.js
+++ pokedex.js
@@ -1,15 +1,22 @@
+// Call and order data from the pokeapi
 fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=10')
 .then((response) => response.json())
-.then((response) => {
+.then(async (response) => {
     console.log(response.results);
-    response.results.forEach(pokemon => fetchPokemon(pokemon.url))
+    for (const pokemon in response.results) {
+        const p = response.results[pokemon];
+        await fetchPokemon(p.url);
+    }
+    
 });
 
+// Apply data from pokeapi to the DOM
 function fetchPokemon(url) {
-    fetch(url).then((response) => {
-    return response.json();
-})
-.then((response) => {
+    return fetch(url)
+    .then((response) => {
+        return response.json();
+    })
+    .then((response) => {
         const types = response.types.map((entry) => entry.type.name);
         const img = response.sprites.front_default;
         const number = response.id;
@@ -30,7 +37,7 @@ function createPokemon(number, name, types, img) {
     const entry = document.querySelector('#pokemon-template').content;
     const pokemon = entry.cloneNode(true); // clone the node
     pokemon.querySelector('.number').innerText = `#${number}`; // DOM-targeting
-    pokemon.querySelector('.name').innerText = name; // DOM-targeting
+    pokemon.querySelector('.name').innerText = name[0].toUpperCase() + name.substring(1);; // DOM-targeting
     pokemon.querySelector('.img-container img').src = img; // DOM-targeting
     
     if(types) { 
@@ -40,7 +47,7 @@ function createPokemon(number, name, types, img) {
             const typeElement = document.createElement("span");
             typeElement.classList.add('type');   
             typeElement.classList.add(`type-${type.toLowerCase()}`);
-            typeElement.innerText = type;
+            typeElement.innerText = type[0].toUpperCase() + type.substring(1);
             typeInfoElement.appendChild(typeElement);
         });
         
