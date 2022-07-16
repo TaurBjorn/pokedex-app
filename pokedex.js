d74af8fb40f13b6ef1f2efb1c3994df90439dc9a
Bjorn Vanmarsenille <bjorn.vanmarsenille90@gmail.com>
2022-07-16 15:50:54 +0200

    added more pokemon with functions, conditionals and loops

--- pokedex.js
+++ pokedex.js
@@ -1,16 +1,35 @@
+function addToPokedex(pokemon) {
+    const pokedex = document.querySelector('.pokedex');
+    pokedex.appendChild(pokemon);
+}
 
-const pokedex = document.querySelector('.pokedex');
-const entry = document.querySelector('.pokedex-entry');
+function createPokemon(number, name, types) {
+    const entry = document.querySelector('#pokemon-template').content;
+    const pokemon = entry.cloneNode(true); // clone the node
+    pokemon.querySelector('.number').innerText = `#${number}`; // DOM-targeting
+    pokemon.querySelector('.name').innerText = name; // DOM-targeting
+    pokemon.querySelector('.img-container img').src = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${number}.png`; // DOM-targeting
+    
+    if(types) { 
+        const typeInfoElement = pokemon.querySelector('.type-info');
+        typeInfoElement.innerHTML = "";
+        types.forEach(type => {
+            const typeElement = document.createElement("span");
+            typeElement.classList.add('type');   
+            typeElement.classList.add(`type-${type.toLowerCase()}`);
+            typeElement.innerText = type;
+            typeInfoElement.appendChild(typeElement);
+        });
+        
+    }
+    
+    return pokemon;
+
+}
 
 
-const cloneOne = entry.cloneNode(true);
-cloneOne.querySelector('.number').innerText = "#002";
-cloneOne.querySelector('.name').innerText = "Ivysaur";
-cloneOne.querySelector('.img-container img').src = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png";
-pokedex.appendChild(cloneOne);
+addToPokedex(createPokemon('001', 'Bulbasaur',  ["Grass", "Poison"]));
+addToPokedex(createPokemon('002', 'Ivysaur', ["Grass", "Poison"]));
+addToPokedex(createPokemon('003', 'Venusaur', ["Grass", "Poison"]));
+addToPokedex(createPokemon('004', 'Charmander', ["Fire"]));
 
-const cloneTwo = entry.cloneNode(true);
-cloneTwo.querySelector('.number').innerText = "#003";
-cloneTwo.querySelector('.name').innerText = "Venusaur";
-cloneTwo.querySelector('.img-container img').src = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png";
-pokedex.appendChild(cloneTwo);
